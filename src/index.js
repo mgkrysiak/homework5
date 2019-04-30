250d6a63aa730428e7e259ceb6e80f6ea3679233
Michalina Krysiak <gabi.krysiak@gmail.com>
2019-04-30 23:17:42 +0200

    fibonacci implementation using recursive function

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function fib(n) {
+
+  const result = [0, 1];
+  for (var i = 2; i < n; i++) {
+    result.push(result[i-2] + result[i-1]);
+  }
+  return result;
+
+};
+
+console.log(fib(8)); 
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  watch: true,
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ]
+  }
+
+};
